dd7ef88e94d1ec2d648ccfe7ad4f989913bf5691
Samim Osman Sabuj <86197373+samimosmansabuj@users.noreply.github.com>
2023-08-27 15:00:06 +0600

    Create Write a program to print sum of 10 natural numbers, 10 even numbers and  10 odd numbers.py

---
+++ Write a program to print sum of 10 natural numbers, 10 even numbers and  10 odd numbers.py
@@ -0,0 +1,31 @@
+# Write a program to print sum of 10 natural numbers, 10 even numbers and  10 odd numbers
+
+# sum of 10 natural numbers
+natural_num_start = 1
+sum_natural_number = 0
+while natural_num_start<=10:
+    sum_natural_number+=natural_num_start
+    natural_num_start+=1
+print("Sum of First 10 Natural Number: ",sum_natural_number)
+
+
+# sum of 10 even numbers
+even_num_start = 2
+sum_even_number = 0
+while even_num_start<=20:
+    sum_even_number+=even_num_start
+    even_num_start+=2
+print("Sum of First 10 Even Number: ",sum_even_number)
+
+
+# sum of 10 even numbers
+odd_num_start = 1
+sum_odd_number = 0
+while even_num_start<=20:
+    sum_even_number+=even_num_start
+    even_num_start+=2
+print("Sum of First 10 Odd Number: ",sum_even_number)
+
+
+
+
