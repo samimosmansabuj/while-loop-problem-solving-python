c9cc46a5c2fc937e6e6226badcf689a792da491d
Samim Osman Sabuj <86197373+samimosmansabuj@users.noreply.github.com>
2023-08-27 14:27:48 +0600

    Add files via upload

---
+++ Write a program to print the following using while loop.py
@@ -0,0 +1,31 @@
+# Write a program to print the following using while loop.
+# First 10 even numbers
+# First 10 odd numbers
+# First 10 Natural Numbers
+# First 10 Whole Numbers
+
+# First 10 even numbers-----------------
+start_even_num = 2
+while start_even_num<=20:
+    print(start_even_num)
+    start_even_num+=2
+
+# First 10 odd numbers
+start_odd_num = 1
+while start_odd_num<=20:
+    print(start_odd_num)
+    start_odd_num+=2
+
+
+# First 10 Natural Numbers
+start_odd_num = 1
+while start_odd_num<=10:
+    print(start_odd_num)
+    start_odd_num+=1
+
+# # First 10 Whole Numbers
+start_odd_num = 0
+while start_odd_num<10:
+    print(start_odd_num)
+    start_odd_num+=1
+
